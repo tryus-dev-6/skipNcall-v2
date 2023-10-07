42eb6413c2edef1cd90f4b954607801bfd2e07f2
dev-6 <tryus.dev6@gmail.com>
2023-10-07 16:31:44 +0600

    1. Add Fund ui updated. 2. Color dart file added. 3. payable lead selected and unselected color added and finish the ui of payabel ui.

--- lib/Pages/PayableLead.dart
+++ lib/Pages/PayableLead.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:skip_n_call/Util/ColorCodes.dart';
 import 'package:skip_n_call/Util/Tools.dart';
 
 
@@ -12,6 +13,22 @@ class PayableLead extends StatefulWidget {
 }
 
 class _PayableLeadState extends State<PayableLead> {
+
+  int warmLeadSelected = 0;
+  int hovLeadSelected = 0;
+  int rawLeadSelected = 0;
+
+  Color warmLeadColor = color0;
+  Color hovLeadColor = color0;
+  Color rawLeadColor = color0;
+
+  Color warmLeadTColor = color2;
+  Color hovLeadTColor = color2;
+  Color rawLeadTColor = color2;
+
+  Color warmLeadBgColor = Colors.white;
+  Color hovLeadBgColor = Colors.white;
+  Color rawLeadBgColor = Colors.white;
 
 
   @override
@@ -41,83 +58,12 @@ class _PayableLeadState extends State<PayableLead> {
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.stretch,
                           children: [
-                            Card(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10.0),
-                              ),
-                              margin: const EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
-                              elevation: 5,
-                              child: Container(
-                                margin: const EdgeInsets.all(10.0),
-                                child: Column(
-                                  children: [
-                                    const Text(
-                                      "Warm Lead",
-                                      softWrap: true,
-                                      overflow: TextOverflow.visible,
-                                      style: TextStyle(
-                                          fontSize: 20, color: Color(0Xff00A18A)),
-                                    ),
-                                    Container(
-                                      margin: const EdgeInsets.only(top: 5),
-                                      child: const Text(
-                                        "Unpaid Lead - 10",
-                                        style: TextStyle(
-                                            fontSize: 15, color: Color(0Xff5A5A5A)),
-                                      ),
-                                    ),
-                                    Container(
-                                      margin: const EdgeInsets.only(top: 5),
-                                      child: const Text(
-                                        "Payable - \$500",
-                                        style: TextStyle(
-                                            fontSize: 15, color: Color(0Xff5A5A5A)),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                            Card(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10.0),
-                              ),
-                              margin: const EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
-                              elevation: 5,
-                              child: Container(
-                                margin: const EdgeInsets.all(10.0),
-                                child: Column(
-                                  children: [
-                                    const Text(
-                                      "Home Owner Verified",
-                                      softWrap: true,
-                                      overflow: TextOverflow.visible,
-                                      style: TextStyle(
-                                          fontSize: 20, color: Color(0Xff00A18A)),
-                                    ),
-                                    Container(
-                                      margin: const EdgeInsets.only(top: 5),
-                                      child: const Text(
-                                        "Unpaid Lead - 10",
-                                        style: TextStyle(
-                                            fontSize: 15, color: Color(0Xff5A5A5A)),
-                                      ),
-                                    ),
-                                    Container(
-                                      margin: const EdgeInsets.only(top: 5),
-                                      child: const Text(
-                                        "Payable - \$500",
-                                        style: TextStyle(
-                                            fontSize: 15, color: Color(0Xff5A5A5A)),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(bottom: 50),
+                            GestureDetector(
+                              onTap: (){
+                                performSelection(1);
+                              },
                               child: Card(
+                                color: warmLeadBgColor,
                                 shape: RoundedRectangleBorder(
                                   borderRadius: BorderRadius.circular(10.0),
                                 ),
@@ -127,30 +73,119 @@ class _PayableLeadState extends State<PayableLead> {
                                   margin: const EdgeInsets.all(10.0),
                                   child: Column(
                                     children: [
-                                      const Text(
-                                        "Raw Lead",
+                                      Text(
+                                        "Warm Lead",
                                         softWrap: true,
                                         overflow: TextOverflow.visible,
                                         style: TextStyle(
-                                            fontSize: 20, color: Color(0Xff00A18A)),
+                                            fontSize: 20, color: warmLeadColor),
                                       ),
                                       Container(
                                         margin: const EdgeInsets.only(top: 5),
-                                        child: const Text(
+                                        child: Text(
                                           "Unpaid Lead - 10",
                                           style: TextStyle(
-                                              fontSize: 15, color: Color(0Xff5A5A5A)),
+                                              fontSize: 15, color: warmLeadTColor),
                                         ),
                                       ),
                                       Container(
                                         margin: const EdgeInsets.only(top: 5),
-                                        child: const Text(
+                                        child: Text(
                                           "Payable - \$500",
                                           style: TextStyle(
-                                              fontSize: 15, color: Color(0Xff5A5A5A)),
+                                              fontSize: 15, color: warmLeadTColor),
                                         ),
                                       ),
                                     ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                            GestureDetector(
+                              onTap: (){
+                                performSelection(2);
+                              },
+                              child: Card(
+                                color: hovLeadBgColor,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                                margin: const EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
+                                elevation: 5,
+                                child: Container(
+                                  margin: const EdgeInsets.all(10.0),
+                                  child: Column(
+                                    children: [
+                                      Text(
+                                        "Home Owner Verified",
+                                        softWrap: true,
+                                        overflow: TextOverflow.visible,
+                                        style: TextStyle(
+                                            fontSize: 20, color: hovLeadColor),
+                                      ),
+                                      Container(
+                                        margin: const EdgeInsets.only(top: 5),
+                                        child: Text(
+                                          "Unpaid Lead - 10",
+                                          style: TextStyle(
+                                              fontSize: 15, color: hovLeadTColor),
+                                        ),
+                                      ),
+                                      Container(
+                                        margin: const EdgeInsets.only(top: 5),
+                                        child: Text(
+                                          "Payable - \$500",
+                                          style: TextStyle(
+                                              fontSize: 15, color: hovLeadTColor),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                            GestureDetector(
+                              onTap: (){
+                                performSelection(3);
+                              },
+                              child: Padding(
+                                padding: const EdgeInsets.only(bottom: 50),
+                                child: Card(
+                                  color: rawLeadBgColor,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                  ),
+                                  margin: const EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
+                                  elevation: 5,
+                                  child: Container(
+                                    margin: const EdgeInsets.all(10.0),
+                                    child: Column(
+                                      children: [
+                                        Text(
+                                          "Raw Lead",
+                                          softWrap: true,
+                                          overflow: TextOverflow.visible,
+                                          style: TextStyle(
+                                              fontSize: 20, color: rawLeadColor),
+                                        ),
+                                        Container(
+                                          margin: const EdgeInsets.only(top: 5),
+                                          child: Text(
+                                            "Unpaid Lead - 10",
+                                            style: TextStyle(
+                                                fontSize: 15, color: rawLeadTColor),
+                                          ),
+                                        ),
+                                        Container(
+                                          margin: const EdgeInsets.only(top: 5),
+                                          child: Text(
+                                            "Payable - \$500",
+                                            style: TextStyle(
+                                                fontSize: 15, color: rawLeadTColor),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
                                   ),
                                 ),
                               ),
@@ -199,5 +234,95 @@ class _PayableLeadState extends State<PayableLead> {
           )
       ),
     );
+  }
+
+  void performSelection(int i) {
+
+    if (i == 1){
+      if(warmLeadSelected == 1){
+
+        setState(() {
+
+          warmLeadColor = color0;
+          warmLeadTColor = color2;
+          warmLeadBgColor = Colors.white;
+
+        });
+
+        warmLeadSelected = 0;
+
+      }
+      else{
+
+        setState(() {
+
+          warmLeadColor = Colors.white;
+          warmLeadTColor = Colors.white;
+          warmLeadBgColor = color1;
+
+        });
+
+        warmLeadSelected = 1;
+
+      }
+    }
+    if(i == 2){
+      if(hovLeadSelected == 1){
+
+        setState(() {
+
+          hovLeadColor = color0;
+          hovLeadTColor = color2;
+          hovLeadBgColor = Colors.white;
+
+        });
+
+        hovLeadSelected = 0;
+
+      }
+      else{
+
+        setState(() {
+
+          hovLeadColor = Colors.white;
+          hovLeadTColor = Colors.white;
+          hovLeadBgColor = color1;
+
+        });
+
+        hovLeadSelected = 1;
+
+      }
+    }
+    if(i == 3){
+
+      if(rawLeadSelected == 1){
+
+        setState(() {
+
+          rawLeadColor = color0;
+          rawLeadTColor = color2;
+          rawLeadBgColor = Colors.white;
+
+        });
+
+        rawLeadSelected = 0;
+
+      }
+      else{
+
+        setState(() {
+
+          rawLeadColor = Colors.white;
+          rawLeadTColor = Colors.white;
+          rawLeadBgColor = color1;
+
+        });
+
+        rawLeadSelected = 1;
+
+      }
+
+    }
   }
 }
